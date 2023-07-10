789abb2ab50a24537564dbd3573d611c125cff45
Gavaskar <gavaskaraeg@gmail.com>
2023-07-10 13:08:27 +0530

    environment

--- config/environment.js
+++ config/environment.js
@@ -79,4 +79,4 @@ export EMAILPASS="Gava050220#"
 export GOOGLE_CLIENT="33502072642-kjgocms4qbvu85u9107o1k2dp3qd7lmu.apps.googleusercontent.com"
 export GOOGLE_SECRET="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"
 export GOOGLE_CALLBACK="http://gavaskar.authsec.ninja/user/auth/google/callback"*/
-module.exports = process.env.Auth_ENVIRONMENT === undefined ? 'development' : process.env.Auth_ENVIRONMENT;
+module.exports = eval(process.env.Auth_ENVIRONMENT) == undefined ? development : eval(process.env.Auth_ENVIRONMENT);
